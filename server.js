fc6a415368944c229cdd3b8a7250d9d102ab9ba1
Jincy Varghese <jincyvar@gmail.com>
2022-06-04 23:39:06 +0530

    test

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const dbConfig = require("./app/config/db.config");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
