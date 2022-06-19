9da040881bbc84e36e86c034e3c4ece71c642243
Jincy Varghese <jincyvar@gmail.com>
2022-06-20 01:12:08 +0530

    sdf

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 //   origin: "http://localhost:3000"
 // };
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
